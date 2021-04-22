852f4e3f844f000b43f65b0455df5c8f3f4c3fd0
jozef.grencik <jozef.grencik@gmail.com>
2021-04-22 22:49:00 +0200

    add first class with some URL params

---
+++ src/Reimage.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Reimage;
+
+class Reimage
+{
+    public const WIDTH = 'w';
+    public const HEIGHT = 'h';
+    public const SIGN = 's';
+
+    private const ALL_PARAMS = [
+        self::WIDTH,
+        self::HEIGHT,
+        self::SIGN,
+    ];
+}
