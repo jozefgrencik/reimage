20ef496cbf3c58f4ea9b230c8a7beabfc3cf6c4f
jozef.grencik <jozef.grencik@gmail.com>
2021-04-29 22:42:22 +0200

    remove session_id because it causes problems on localhost

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,4 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Fixate session_id early on, as php7.2+ does not allow the session_id to be set
 // after stdout has been written to.
-session_id('cli');
+// session_id('cli'); //???
