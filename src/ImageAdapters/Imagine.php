750b0ca46bd040a8b6ebcba07388c33389db6032
Jozef Grencik <20k121@gmail.com>
2021-05-22 20:05:58 +0200

    add support for imagine/imagine

---
+++ src/ImageAdapters/Imagine.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Reimage\ImageAdapters;
+
+use Exception;
+use Imagine\Image\Box;
+use Reimage\Exceptions\ReimageException;
+use Reimage\ImageAdapters\ImageInterface as ReimageImageInterface;
+use Imagine\Image\AbstractImagine;
+use Imagine\Image\ImageInterface;
+
+class Imagine implements ReimageImageInterface
+{
+    /** @var ImageInterface */
+    private $imageObject;
+
+    public function isInstalled(): bool
+    {
+        return class_exists(AbstractImagine::class);
+    }
+
+    public function loadImage(string $realPath): void
+    {
+        try {
+            $imagine = new \Imagine\Imagick\Imagine();
+        } catch (Exception $e) {
+            $imagine = new \Imagine\Gd\Imagine();
+        }
+
+        $this->imageObject = $imagine->open($realPath);
+    }
+
+    public function getImageObject(): ImageInterface
+    {
+        return $this->imageObject;
+    }
+
+    public function getImageString(): string
+    {
+        return $this->imageObject->get('jpg'); //todo fix extension
+    }
+
+    public function resize(?int $width = null, ?int $height = null): void
+    {
+        if ($width === null || $height === null) {
+            throw new ReimageException('Currently unsupported'); //todo fix
+        }
+        $this->imageObject->resize(new Box($width, $height));
+    }
+
+    public function rotate(float $angle): void
+    {
+        $this->imageObject->rotate((int)round($angle));
+    }
+}
